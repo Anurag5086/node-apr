e5ee0662f8b35647965ee3cd5927e0bf60722ec4
Anurag5086 <anuraggupta5086@gmail.com>
2026-04-29 21:12:23 +0530

    auth done

--- ecommerce-app/backend/routes/authRoutes.js
+++ ecommerce-app/backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { registerUser, verifyOtp, resendOtp } = require('../controllers/auth.controller');
+const { registerUser, verifyOtp, resendOtp, loginUser } = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/register', registerUser);
-// router.post('/login', loginUser);
+router.post('/login', loginUser);
 router.post('/verify-otp', verifyOtp);
 router.post('/resend-otp', resendOtp);
 
